18b6aeadd444f211d1f080d6ff48bdd337fb6761
rizts <rizts.tech@gmail.com>
2026-05-14 12:52:53 +0700

    fix: call GET/POST methods explicitly on auth handler to fix type error

--- frontend/app/api/auth/[...all]/route.ts
+++ frontend/app/api/auth/[...all]/route.ts
@@ -6,10 +6,10 @@ const handler = toNextJsHandler(auth);
 
 export const GET = async (req: NextRequest) => {
     console.log("Auth GET Request:", req.url);
-    return await handler(req);
+    return await handler.GET(req);
 };
 
 export const POST = async (req: NextRequest) => {
     console.log("Auth POST Request:", req.url);
-    return await handler(req);
+    return await handler.POST(req);
 };
